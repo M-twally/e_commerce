a9c1724d6642aa9964738ac32705c508d65678a6
M_twally <m.ametwally15@gmail.com>
2023-09-22 19:31:12 +0300

    db online connection

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const connectDB=async ()=>{
-   return await mongoose.connect(process.env.DB_URL).then(result=>{
+   return await mongoose.connect(process.env.DB_ONLINE).then(result=>{
     console.log(`DB connected`)
     //console.log(result)
 }).catch(err=>{
