518010b6f0262e325deee057085b5aeb6a1f8216
M_twally <m.ametwally15@gmail.com>
2023-09-25 02:02:45 +0300

    checkout new routes

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -6,7 +6,9 @@ const router=Router()
 router.get("/",authentication,OrderRouter.getspecificorder)
 router.get("/all",OrderRouter.getallorders)
 router.post("/order/:id",authentication,OrderRouter.createCashOrder)
-router.post("/checkout/:id",authentication,OrderRouter.createcheckoutsession)
+// router.post("/checkout/:id",authentication,OrderRouter.createcheckoutsession)
+router.post("https://e-commerce-8yxq.onrender.com/:id",authentication,OrderRouter.createcheckoutsession)
+
 
 
 
